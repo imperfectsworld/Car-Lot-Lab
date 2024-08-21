458d3ff7b4a7d576e9efd4a0601885a3a1f0ab16
imperfectsworld <imperfectsworld@gmail.com>
2024-08-21 15:24:01 -0400

    Created the car class

---
+++ Car Lot Lab/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Lot_Lab
+{
+    internal class Car
+    {
+        //parameters 
+
+        public string Make {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //constructor
+
+        public Car()
+
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0;
+
+        }
+
+        public Car(string _make, string _model, int _year, decimal _price)
+
+        {
+            Make = _make;
+            Model = _model;
+            Year = _year;
+            Price = _price;
+
+        }
+
+        //methods
+
+        public override string ToString()
+        {
+
+            return ($"{Make}\t\t{Model}\t\t{Year}\t\t{Price}");
+        }
+    }
+}
+
